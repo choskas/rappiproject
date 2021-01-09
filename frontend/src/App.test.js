f526f631005157a531bbb371b423e8ef9fc98e15
Choskas Delta <Choskas Delta>
2021-01-08 20:26:42 -0600

    add

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import numIslands from './components/numberOfIslands';
+test('Island test', () => {
+  const arrTest =  [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+  ]
+  expect(numIslands(arrTest)).toBe(1)
+})
